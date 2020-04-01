4d4882ec4e664def7c55ffbc0935fd0204d1d083
胡若琛 <82155551@qq.com>
2020-04-01 21:32:02 +0800

    初始化

---
+++ src/main/java/com/neu/OurleetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.neu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurleetcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurleetcodeApplication.class, args);
+	}
+
+}
